baac8b634ec4d38e6c2fe687b503169dd4d7586e
MemittoAlvarado <memitto.alvarado@gmail.com>
2021-03-09 17:16:53 -0600

    Se agregaron secciones a Inicio y se paso calculadora a inicio tambien

--- src/components/inicio.vue
+++ src/components/inicio.vue
@@ -22,6 +22,7 @@
           </v-carousel-item>
         </v-carousel>
       </div>
+      
       <v-content  v-for="(id,i) in items" :key="i" v-show="id.image"  >
         <div class="carrusel"  :style="id.style" >
         <H2>Opción 2</H2>
@@ -41,9 +42,69 @@
         </v-row>     
         </div>  
       </v-content>
+
+      <template>
+        <v-container>
+            <v-card-title> Sección Plataforma </v-card-title><br>
+        </v-container>
+      </template>
+
+      <template>
+        <v-container>
+            <v-card-title> Seccion Experiencia </v-card-title><br>
+        </v-container>
+      </template>
+
+      <template>
+        <v-container>
+            <v-card-title> Sección Caracteristicas </v-card-title><br>
+        </v-container>
+      </template>
+
+      <template>
+          <v-container>
+              <v-container >
+                  <v-flex>
+                          <v-card-title>
+                              Calculadora
+                          </v-card-title><br>
+                  </v-flex>
+              </v-container>
+              <v-card>
+                  <v-row justify="center">
+
+                      <div class="columns medium-4" v-for="pet in petsLIst" :key="pet.Nombre">
+                        <v-flex> 
+                          <p> {{pet.Nombre}}</p>
+                        </v-flex>
+
+                          <div v-for="carac in pet.Caracteristicas" :key="carac.Nombre">
+                              {{carac.Nombre}}
+                              <div v-for="precios in carac.Opciones" :key="precios.Nombre">
+                                  <v-checkbox :label="precios.Nombre" v-model="precios.id" @click="action2(precios)">
+                                  </v-checkbox>
+                              </div>
+                          </div>
+                      </div>
+                  </v-row>
+              </v-card>
+              <v-row>
+                  <v-col cols="4">
+                      <v-subheader>Total</v-subheader>
+                  </v-col>
+                  <v-col cols="8">
+                      <v-text-field
+                      label="Resultado"
+                      class="Resultado"
+                      v-model="Final">
+                      </v-text-field>                              
+                  </v-col>
+              </v-row>
+          </v-container>
+      </template>
+
     </v-container>
 </template>
-
 
 <script>
   export default {
@@ -52,6 +113,8 @@
         aux: true,
         aux2: false,
         aux3: false,
+        final2:0,
+        Final:0,
         items: [
           {
             image:false,
@@ -81,7 +144,55 @@
             titulo: 'Baño',
             descripcion: 'Descripcion del servicio de baño'
           }
-        ]
+        ],
+        petsLIst: 
+        [
+            {
+                Nombre: 'Corte de de pelo' ,
+                Caracteristicas: 
+                [
+                    {
+                        Nombre: "Tamaño",
+                        Opciones: 
+                        [
+                            {
+                                Nombre :"Chico",
+                                id: false,
+                                Valor : 30
+                            },
+                            {
+                                Nombre : "Grande",
+                                id: false,
+                                Valor : 80
+                            }
+                        ]
+                    }
+                ],
+            },
+            {
+                Nombre:'Baño',
+                id: false,
+                Caracteristicas: 
+                [
+                    {
+                        Nombre: "Tipo de baño",
+                        Opciones: 
+                        [
+                            {
+                                Nombre :"Sencillo",
+                                id: false,
+                                Valor : 150
+                            },
+                            {
+                                Nombre : "Completo",
+                                id: false,
+                                Valor : 350
+                            }
+                        ]
+                    }
+                ],
+            }            
+        ],
       }
     },
     methods:{
@@ -89,7 +200,34 @@
         console.log(opcion)
         this.items[opcion].image = false
         this.items[(opcion + 1) % 4].image = true
-      }
+      },
+       action (Servicio,Caracteristica,Opcion) {
+            var aux= this.petsLIst.find(
+                function(a) {
+                    return a.Nombre == Servicio;
+                }
+            )
+            var aux2= aux.Caracteristicas.find(
+                function(a) {
+                    return a.Nombre === Caracteristica;
+                }
+            )
+            var aux3= aux2.Opciones.find(
+                function(a) {
+                    return a.Nombre === Opcion;
+                }
+            )  
+            console.log(aux3.Valor)          
+            if(aux3.id===true){
+                this.Final = this.Final + aux3.Valor
+            }else{
+                this.Final = this.Final - aux3.Valor
+            }
+            
+        },
+        action2(jsonOpcionServicio){
+            this.Final+=(jsonOpcionServicio.id?1:-1)*jsonOpcionServicio.Valor;
+        }
     }
   }
 </script>
