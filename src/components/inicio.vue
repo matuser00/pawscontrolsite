629ed61f423dafa5056a76e3a021cce9249f35e8
luissangines <luis.sangines.martinez@gmail.com>
2021-03-08 18:03:26 -0600

    Actualización inicio.vue

--- src/components/inicio.vue
+++ src/components/inicio.vue
@@ -2,28 +2,29 @@
 
     <v-container>
       <div>
-        <H2>Otra Opción</H2>
-        <v-card>
-              <v-content >
-                  <v-img
-                    height="600"
-                    src="https://image.freepik.com/foto-gratis/perro-siendo-revisado-veterinario-tienda-mascotas_23-2148872561.jpg"
-                  >
-                  </v-img>       
-                  <v-row  align="start" justify="end" 
-        >
-                    <v-card class="transparent"
-                      max-width="400"
-                    >
-                    <v-card-title class="title">Titulo de las imagenes </v-card-title>
-                    </v-card>
-                  </v-row>
+        <H2>Opción 1</H2>
+        <v-carousel cycle style="height: 600px;" hide-delimiter-background show-arrows-on-hover>
+          <template v-slot:prev="{ on, attrs }">
+            <div color="transparent" v-for="(id,i) in items" :key="i" v-show="id.image" >
+              <v-card-title color="transparent" class="title" v-bind="attrs" v-on="on">
+                {{id.titulo}} 
+              </v-card-title>
+            </div>
+          </template>
+          <template v-slot:next="{ on, attrs }">
+              <v-content  v-for="(id,i) in items" :key="i" v-show="id.image"  >
+                <v-btn color="info" v-bind="attrs" v-on="on">Next</v-btn>
               </v-content>
-        </v-card>
+          </template>
+          <v-carousel-item v-for="(item,i) in items" :key="i" :src="item.src" >
+              <v-row class="fill-height" align="center" justify="center">
+              </v-row>
+          </v-carousel-item>
+        </v-carousel>
       </div>
       <v-content  v-for="(id,i) in items" :key="i" v-show="id.image"  >
-        <div class="carrusel" id="carrusel" >
-        {{id}}{{i}}
+        <div class="carrusel"  :style="id.style" >
+        <H2>Opción 2</H2>
         <v-row align="center" justify="end" style="height: 600px;" >
           <v-col  md="8">
              <v-card class="transparent"
@@ -51,38 +52,36 @@
         aux: true,
         aux2: false,
         aux3: false,
-        shows: [{image:false},{image:false},{image:true}],
         items: [
           {
-            image:true,
-            src: 'https://image.freepik.com/foto-gratis/perro-siendo-revisado-veterinario-tienda-mascotas_23-2148872561.jpg',
+            image:false,
+            src:'https://image.freepik.com/foto-gratis/perro-siendo-revisado-veterinario-tienda-mascotas_23-2148872561.jpg',
+            style: 'background:url(https://image.freepik.com/foto-gratis/perro-siendo-revisado-veterinario-tienda-mascotas_23-2148872561.jpg);background-repeat:no-repeat;background-size: cover;',
             titulo: 'PetMascota',
             descripcion: 'Descripcion del servicio'
           },
           {
             image:false,
-            src: 'https://image.freepik.com/vector-gratis/hospital-moderno-perros-gatos_74855-7620.jpg',
+            src:'https://image.freepik.com/vector-gratis/hospital-moderno-perros-gatos_74855-7620.jpg',
+            style: 'background:url(https://image.freepik.com/vector-gratis/hospital-moderno-perros-gatos_74855-7620.jpg);background-repeat:no-repeat;background-size: cover;',
             titulo: 'Servicios',
             descripcion: 'Descripcion del servicio en general'
           },
           {
             image:false,
             src: 'https://image.freepik.com/foto-gratis/mujer-afroamericana-positiva-sostiene-perro-miniatura-obediente-manos-pasa-dia-libre-su-mascota-favorita-animal-comprado-tienda-mascotas-aislado-sobre-fondo-amarillo_273609-34264.jpg',
+            style: 'background:url(https://image.freepik.com/foto-gratis/mujer-afroamericana-positiva-sostiene-perro-miniatura-obediente-manos-pasa-dia-libre-su-mascota-favorita-animal-comprado-tienda-mascotas-aislado-sobre-fondo-amarillo_273609-34264.jpg);background-repeat:no-repeat;background-size: cover;',
             titulo: 'Corte de pelo',
             descripcion: 'Los perros pueden contar con dos capas de pelo: el pelo profundo y el pelo protector. Ambos nacen de un mismo folículo. El profundo es el pelo más corto y cercano a la piel, suave y fino, que actúa como aislante. El pelo protector se rodea de varios pelos profundos y es el que forma la capa superior del manto del animal y ayuda a prevenir lesiones.'
           },
           {
-            image:false,
-            src: 'https://image.freepik.com/foto-gratis/joven-mujer-caucasica-perro-sobre-pared-aislada_1368-114726.jpg',
+            image:true,
+            src:'https://image.freepik.com/foto-gratis/joven-mujer-caucasica-perro-sobre-pared-aislada_1368-114726.jpg',
+            style: 'background:url(https://image.freepik.com/foto-gratis/joven-mujer-caucasica-perro-sobre-pared-aislada_1368-114726.jpg);background-repeat:no-repeat;background-size: cover;',
             titulo: 'Baño',
             descripcion: 'Descripcion del servicio de baño'
           }
-        ],
-        alignments: [
-        'start',
-        'center',
-        'end',
-      ],
+        ]
       }
     },
     methods:{
@@ -90,9 +89,6 @@
         console.log(opcion)
         this.items[opcion].image = false
         this.items[(opcion + 1) % 4].image = true
-        console.log(this.items[(opcion + 1) % 4].src)
-        document.getElementById("carrusel").style.background = "url("+this.items[(opcion + 1) % 4].src+")";
-
       }
     }
   }
@@ -111,7 +107,7 @@
    color:#009688ff
  }
 
- #carrusel{
+ .carrusel{
   margin:10px auto;
   border-radius:5px;
   border: 1px solid #999;
@@ -119,8 +115,5 @@
   width:100%;
   height:650px;
   background-size: 100% 100%;
-  background-repeat:no-repeat;
-  background:url('https://image.freepik.com/foto-gratis/perro-siendo-revisado-veterinario-tienda-mascotas_23-2148872561.jpg');
-  background-size: cover;
  }
 </style>
